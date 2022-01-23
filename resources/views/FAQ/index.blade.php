63a1e23416447625a336ad347fed4b38286c6aa9
Elwin Man <man0008@hz.nl>
2022-01-23 16:30:03 +0100

    added FAQ page

---
+++ resources/views/FAQ/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title')
+    FAQ
+@endsection
+
+@section('content')
+    <main>
+        <h1>@yield('title')</h1>
+        <p>Waarvoor is dit website gemaakt?</p><br>
+        <p>Omdat het voor CIN moet.</p>
+    </main>
+@endsection
