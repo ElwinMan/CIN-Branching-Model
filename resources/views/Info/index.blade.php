117c368252d788d14b951b169e9401843c4b59f2
Luca Geene <geen0020@hz.nl>
2022-01-23 23:27:25 +0100

    Fixed the info page; didnt load in correctly

--- resources/views/Info/index.blade.php
+++ resources/views/Info/index.blade.php
@@ -11,6 +11,6 @@
             <p>Deze applicatie is speciaal gemaakt voor CI. Veel plezier.</p>
         <br>
         <p>Wil je zelf meer info toevoegen? Dat kan hier!</p>
-        <h3><a href="{{ route('FAQ.create') }}">Create New FAQ</a></h3>
+  
     </main>
 @endsection
